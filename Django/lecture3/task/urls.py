df65443fb83115b6f4c2e0d9ed52241084534468
Rodrigo Santiago <33rodrigosantiago@gmail.com>
2020-07-03 12:13:31 -0300

    Adicionado as funcionalidades de adicionar a Lista e também a implementação de sessões simples através do Cokie do navegador, então cada user tem uma lista vazia iniciada na primeira vez e salva os itens separadamente.

--- Django/lecture3/task/urls.py
+++ Django/lecture3/task/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+#Define um nome para evitar namespace C.
+app_name = 'tasks'
+
 urlpatterns = [
     path("", views.index, name="index"),
     path("addTask", views.addTask, name="addTask")
--- Django/lecture3/task/views.py
+++ Django/lecture3/task/views.py
@@ -1,15 +1,66 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django import forms
+from django.urls import reverse
 
-# Create your views here.
+#Classe para os Formulários Django
 
-task = ['Rodrigo', 'Santiago', 'Claro', 'Filho']
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="Nova Tarefa")
+    #priodidade = forms.IntegerField(label="Prioridade", min_value=1, max_value=10)
+
+
+#------------------------------------------#
+#iniciando uma Lista vazia (Aqui é o caso sem Sessões, bem simples)
+#tasks = [] #Comentado pois vou implementar Sessão de usuário
+#------------------------------------------#
+
+#Criando as sessões de usuário (dentro da função Index)
 
 def index(request):
+    # Aqui eu verifico se existe alguma "tasks" na sessão deste usuário (usando os cookies dele)
+    # Caso não, irei iniciar a "tasks" vazia
+    if "tasks" not in request.session:
+        request.session["tasks"] = []
     
-    return render(request, 'task/index.html', {'tasks': task})
+    #------------------------------------------#
+    #Caso antigo sem sessão
+    #return render(request, 'task/index.html', {'tasks': tasks})
+    #------------------------------------------#
 
+    #Com Sessão
+    return render(request, 'task/index.html', {
+        "tasks": request.session["tasks"]
+    })
 
 def addTask(request):
+    #Se o method de envio for POST
+    if request.method == "POST":
+        #Cria uma variavel e recebe tudo o que o usuário digitou
+        form = NewTaskForm(request.POST)
+        #Verifica se é valido
+        if form.is_valid():
+            #Pega os valores e armazena em taks
+            task = form.cleaned_data["task"]
 
-    return render(request, 'task/addTask.html', {'tasks': task})
+            #------------------------------------------#
+            #Novamente forma antiga
+            #Da um Append na Lista com os valores recebidos
+            #task.append(task)
+            #------------------------------------------#
+
+            request.session["tasks"] += [task] #Adiciona na lista da sessão
+            
+            #Retorna através do HTTP para a o html que lista
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            #Não atendeu as "Regras" do servidor quando atualizamos, aqui
+            #ele vai retornar o erro e o formulário
+            return render(request, "task/addTask.html",{
+                "form": form
+            })
+    # Aqui é o caso da conexão direta, somente cria o form em branco para o usuário
+    return render(request, 'task/addTask.html', {
+        "form": NewTaskForm()
+    })
     
